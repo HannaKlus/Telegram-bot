818d287a812e2d3bf654faf76edc64337531bf60
Hanna Klus <hania.klus@gmail.com>
2020-04-04 22:33:57 +0200

    added corona cases in CZ

--- bot.py
+++ bot.py
@@ -36,26 +36,23 @@ def cat(bot, update):
     bot.send_message(chat_id=chat_id, text="Meow " + name)
 
 def zara(bot, update):
-	zarzine_fotki = ["https://www.instagram.com/p/Bdf4AKIgsmg/", "https://www.instagram.com/p/Bj5UwStAfee/", "https://www.instagram.com/p/Bdf51JLgJLj/", "https://www.instagram.com/p/Bdf43zDALaC/"]
-	url = random.choice(zarzine_fotki)
-	name = get_name(update)
-	chat_id = update.message.chat_id
-	bot.send_photo(chat_id=chat_id, photo=url)
-	bot.send_message(chat_id=chat_id, text="Haf " + name)
+    zarzine_fotki = ["https://www.instagram.com/p/Bdf4AKIgsmg/", "https://www.instagram.com/p/Bj5UwStAfee/", "https://www.instagram.com/p/Bdf51JLgJLj/", "https://www.instagram.com/p/Bdf43zDALaC/"]
+    url = random.choice(zarzine_fotki)
+    name = get_name(update)
+    chat_id = update.message.chat_id
+    bot.send_photo(chat_id=chat_id, photo=url)
+    bot.send_message(chat_id=chat_id, text="Haf " + name)
 
 def korona(bot, update):
-	try:
-		print("W koronie")
-		# url = "https://onemocneni-aktualne.mzcr.cz/covid-19"
-		url = "http://bit.ly/3231YVs"
-		contents = requests.get('https://onemocneni-aktualne.mzcr.cz/covid-19').json()
-		print(contents) 
-		name = get_name(update)
-		chat_id = update.message.chat_id
-		bot.send_photo(chat_id=chat_id, photo=url)
-		bot.send_message(chat_id=chat_id, text="Haf " + name)
-	except Error as e:
-		print("An exception occurredddd", e)
+    try:
+        print("W koronie")
+        contents = requests.get('https://onemocneni-aktualne.mzcr.cz/api/v1/covid-19/nakaza.min.json').json()
+        chat_id = update.message.chat_id
+        celkem=str(contents[-1]['pocetCelkem'])
+        den=str(contents[-1]['pocetDen'])
+        bot.send_message(chat_id=chat_id, text='Day: ' + den + ' total: ' + celkem)
+    except Error as e:
+        print("An exception occurredddd", e)
 
 
 def main():
@@ -64,7 +61,7 @@ def main():
     dp.add_handler(CommandHandler('dejpiesa',dog))
     dp.add_handler(CommandHandler('dejkota',cat))
     dp.add_handler(CommandHandler('dejzare',zara))
-    # dp.add_handler(CommandHandler('korona',korona))
+    dp.add_handler(CommandHandler('korona',korona))
     updater.start_polling()
     updater.idle()
     
